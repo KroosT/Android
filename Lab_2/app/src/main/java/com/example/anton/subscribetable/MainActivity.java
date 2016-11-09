216769b08d20d449db0ab590805abcd5d384ee63
KroosT <anton.shestak.96@gmail.com>
2016-11-09 20:13:57 +0300

    Style changes.

--- Lab_2/app/src/main/java/com/example/anton/subscribetable/MainActivity.java
+++ Lab_2/app/src/main/java/com/example/anton/subscribetable/MainActivity.java
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     public void BtnSubscribeClick(View v) {
         LinearLayout chb_layout = (LinearLayout) findViewById(R.id.chb_layout);
         Editable checkBoxText = ((EditText) findViewById(R.id.name_entry)).getText();
+        ((EditText) findViewById(R.id.email_entry)).setText("");
+        ((EditText) findViewById(R.id.name_entry)).setText("");
         CreateCheckBoxes(chb_layout, count, checkBoxText, false);
         count++;
         Toast.makeText(this, "Subscribe activated successfully!", Toast.LENGTH_SHORT).show();
